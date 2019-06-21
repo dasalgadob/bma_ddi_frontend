c2a4fc636a8ada9152dc8cf8469e6ec5aa53cb56
Diego Salgado <dasalgadob@unal.edu.co>
2019-06-21 02:05:55 -0500

    Closes #2 React router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Interviews from './views/interviews/index.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      <Link to="/interviews">Interviews</Link>
+      </nav>
+      <div>
+        <Route path="/interviews" component={Interviews} />
+      </div>
     </div>
   );
 }
