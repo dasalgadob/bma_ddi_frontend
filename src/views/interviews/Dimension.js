7168a8154b2b95009a378352cae9568a637a9793
Diego Salgado <dasalgadob@unal.edu.co>
2019-07-16 14:30:39 -0500

    Dimension with table style for better visualization

--- src/views/interviews/Dimension.js
+++ src/views/interviews/Dimension.js
@@ -4,33 +4,33 @@ export default class Dimension extends Component{
 
 
     render(){
-        const style = {fontSize: '14px'};
+        const style = {fontSize: '15px'};
         console.log('this.props');
         console.log(this.props);
         return (
-            <div class="container mt-1">
+            <div class="container">
                 <h3 className="text-left">{this.props.name.spanish}</h3>
                 <p className="text-left">{this.props.description.spanish}</p>
                 <div >
-                    <div class="row card " style={style}>
-                        <tbody>
+                    <div className="card text-left" style={style}>
+                        <table className="table table-striped">
+                            <tbody className="">
                     {this.props.questions.map(
                         (q) => 
-                            <tr>
-                            <div class="form-check ml-5 mr-5">
-                                <td>
-                            <input key={q.id} class="mr-2 " type="checkbox" value="" id={q.id} />
-                            </td>
-                            <td>
-                            <label class="" for="defaultCheck1">
-                                {q.attributes.translation.spanish}
-                            </label>
-                            </td>
+                            <tr >
+                                <td className="">
+                            <div className="custom-control custom-checkbox">
+                                <input id={q.id} key={q.id} className="custom-control-input" type="checkbox" value="true" >
+                                    </input>
+                                <label className="custom-control-label" for={q.id}>
+                                    {q.attributes.translation.spanish}
+                                </label>
                             </div>
+                            </td>
                             </tr>
-                        
                     )}
                     </tbody>
+                    </table>
                     </div>
                 </div>
             </div>
