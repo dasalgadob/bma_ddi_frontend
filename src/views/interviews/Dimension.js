b0a959e5a042a49d1b0dee107bee96e59627a174
Diego Salgado <dasalgadob@unal.edu.co>
2019-07-17 18:13:01 -0500

    Making the Dimension component update state of Interview

--- src/views/interviews/Dimension.js
+++ src/views/interviews/Dimension.js
@@ -2,27 +2,37 @@ import React,{ Component} from "react";
 
 export default class Dimension extends Component{
 
+    constructor(props){
+        super(props);
+        this.onQuestionSelected = this.onQuestionSelected.bind(this);
+    }
+
+    onQuestionSelected(e){
+        this.props.onClick(e);
+    }
 
     render(){
         const style = {fontSize: '15px'};
         console.log('this.props');
         console.log(this.props);
         return (
-            <div class="container">
-                <h3 className="text-left">{this.props.name.spanish}</h3>
-                <p className="text-left">{this.props.description.spanish}</p>
+            <div className="mt-4">
+                <h3 className="text-left ml-5">{this.props.name.spanish}</h3>
+                <p className="text-left ml-5">{this.props.description.spanish}</p>
                 <div >
-                    <div className="card text-left" style={style}>
+                    <div className="card text-left mx-4" style={style}>
                         <table className="table table-striped">
                             <tbody className="">
                     {this.props.questions.map(
                         (q) => 
-                            <tr >
+                            <tr key={"tr-" + q.id}>
                                 <td className="">
                             <div className="custom-control custom-checkbox">
-                                <input id={q.id} key={q.id} className="custom-control-input" type="checkbox" value="true" >
+                                <input id={q.id} key={q.id} className="custom-control-input" type="checkbox" value="true"
+                                       onClick={this.onQuestionSelected}
+                                       name={q.id} >
                                     </input>
-                                <label className="custom-control-label" for={q.id}>
+                                <label className="custom-control-label" htmlFor={q.id}>
                                     {q.attributes.translation.spanish}
                                 </label>
                             </div>
