bcba111c315c7997ad99e0ab177212e5b5ddec66
Diego Salgado <dasalgadob@unal.edu.co>
2019-07-22 01:12:40 -0500

    Refactoring Candidate creation form to be handle by one function

--- src/views/candidates/Candidate.js
+++ src/views/candidates/Candidate.js
@@ -1,32 +1,38 @@
 import React, {Component} from 'react';
 
-
 export default class Candidate extends Component{
 
     constructor(props){
         super(props);
+    }
 
+    handleSubmit = (e) => {
+        this.props.onSubmit(e);
     }
 
     render(){
-        return(<div class="">
-            <form>
+        return(<div className="">
+            <form onSubmit={this.handleSubmit}>
             <div className="form-group row">
-                    <label for="exampleInputPassword1" className="align-left">Nombre</label>
+                    <label htmlFor="exampleInputPassword1" className="align-left">Nombre</label>
                     <input type="text"
+                           name="name"
                            className="form-control" 
                            id="exampleInputPassword1" 
                            placeholder="Nombre"
-                           required="required"/>
+                           required="required"
+                           onChange={this.props.onInputChange}/>
                 </div>
                 <div className="form-group row">
-                    <label for="exampleInputEmail1" className="align-left">Email</label>
+                    <label htmlFor="exampleInputEmail1" className="align-left">Email</label>
                     <input type="email" 
+                           name="email"
                            className="form-control" 
                            id="exampleInputEmail1" 
                            aria-describedby="emailHelp" 
                            placeholder="Enter email"
-                           required="required" />
+                           required="required"
+                           onChange={this.props.onInputChange} />
                 </div>
                 
                 <button type="submit" className="btn btn-primary">Guardar</button>
