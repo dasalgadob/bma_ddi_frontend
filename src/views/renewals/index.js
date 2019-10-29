f4070a531168ff99098f5df7080652dae94c26cd
Diego Salgado <dasalgadob@unal.edu.co>
2019-10-29 05:31:57 -0500

    Fixed title of Renewals on indes.js

--- src/views/renewals/index.js
+++ src/views/renewals/index.js
@@ -10,7 +10,7 @@ class  RenewalsList extends Component {
         const { t, i18n } = this.props;
         return(
             <div>
-                <h2 className="mb-3 mt-2">{t('interviews.title_lower')}</h2>
+                <h2 className="mb-3 mt-2">{t('renewals.title')}</h2>
                 <div className="container">
                     <div className="row align-items-left">
                     <Link to='renewalsList/new' className=" btn btn-primary">
