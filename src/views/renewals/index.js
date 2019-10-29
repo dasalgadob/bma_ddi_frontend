7faf519edc0f1bf18469ccd707931614cf7ef767
Diego Salgado <dasalgadob@unal.edu.co>
2019-10-29 05:29:07 -0500

    Closes #59 button new renewals list

--- src/views/renewals/index.js
+++ src/views/renewals/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 class  Renewals extends Component {
 
     render(){
+        const { t, i18n } = this.props;
         return(
-            <div>Renewals</div>
+            <div>
+                <h2 className="mb-3 mt-2">{t('interviews.title_lower')}</h2>
+                <div className="container">
+                    <div className="row align-items-left">
+                    <Link to='renewalsList/new' className=" btn btn-primary">
+                        <FontAwesomeIcon icon={faPlus} /> {t('renewals.create')}
+                    </Link>
+                    </div>
+                    
+                </div>
+            </div>
         );
 
     }
